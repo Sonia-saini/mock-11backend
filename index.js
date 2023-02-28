2ffe35b92ec75338b59484bae638f43e6da81d07
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-02-28 09:53:13 +0530

    sdfs

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { connection } = require("./Configs/db");
 const { userRouter } = require("./Routes/UserRoutes");
 const cors=require("cors")
 const app=express();
-app.use(express.json())
 app.use(cors());
+
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("welcome home")
