10a1769c03cba4b5a7ab8626bcaec0c31af6b150
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-03-02 13:03:07 +0530

    asdfsd

--- Modues/usermodel.js
+++ Modues/usermodel.js
@@ -1,11 +1,8 @@
 const mongoose=require("mongoose");
 const userschema=mongoose.Schema({
     name : {type: String, required: true},
-    bio:{type:String},
-    phone:{type:String},
-    url:{type:String},
-    email : {type: String, required: true},
-    password : {type: String, required: true}, 
+    score:{type:Number, required:true},
+    difficulty:{type:String, required:true},
 })
-const Usermodel=mongoose.model("user",userschema);
+const Usermodel=mongoose.model("userquiz",userschema);
 module.exports={Usermodel}
